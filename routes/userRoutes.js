5f0b7f1d14bbd86e4bce78f33b36c401c9fe42f0
Abhishek Patil <abhishekp.7841@gmail.com>
2025-04-05 10:49:30 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 // Routes for user-related API endpoints
 const express = require("express");
 const router = express.Router();
-const { getUser } = require("../controllers/userController");
+const userController = require("../controllers/userController");
 const cacheMiddleware = require("../middleware/cacheMiddleware");
 
-router.get("/user/:id", cacheMiddleware, getUser);
+router.get("/user/:id", cacheMiddleware, userController.getUser);
 
 module.exports = router;
